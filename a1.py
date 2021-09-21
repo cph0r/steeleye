f00b0aeb2d39fd092dd5d217db5e39649e33ba23
cph0r <chiragphor19@gmail.com>
2021-09-21 20:25:28 +0530

    new changes

--- a1.py
+++ a1.py
@@ -2,63 +2,97 @@ import requests
 import pandas as pd
 from constants import *
 import os
+import logging
+import datetime
+logging.basicConfig(filename='assessment1.log', level=logging.DEBUG)
 
-def get_repos(repo_count, max_commit_count):
-    headers = {AUTHERIZATION: AUTH_TOKEN+TOKEN_VALUE}
-    r = requests.get(URL,headers).json()
-    df = pd.DataFrame.from_records(r)[0:repo_count]
-    owner = df[OWNER]
-    df[OWNER_TYPE] = owner.apply(lambda x: x[TYPE])
-    df[OWNER_URL] = owner.apply(lambda x: x[HTML_URL])
-    df[OWNER_USERNAME] = owner.apply(lambda x: x[LOGIN])
-    df[OWNER_ADMIN] = owner.apply(lambda x: x[SITE_ADMIN])
+def make_api_call(repo_count, max_commit_count):
+    r = requests.get(URL, HEADERS).json()
+    try:
+        df = pd.DataFrame.from_records(r)[0:repo_count]
+        create_repos_csv(df, max_commit_count)
+    except Exception as ex:
+        logging.error(str(datetime.datetime.now())+':'+API_LIMIT)
 
-    df = df[[ID,NAME,FORK ,FULL_NAME,OWNER_URL,OWNER_USERNAME,OWNER_ADMIN,
-    PRIVATE, DESCRIPTION, NODE_ID, HTML_URL,OWNER_TYPE,COMMITS_URL]]
+
+def create_repos_csv(df, max_commit_count):
+    """Create Repos Csv"""
+    for key, value in DF_MAP.items():
+        add_new_col(key, value, df[OWNER], df)
+    df = df[REPO_COLUMNS]
     
-    
-    k=0
-    for url in df[COMMITS_URL]:
-        url = url.replace('{/sha}','')
-        response = requests.get(url,headers).json()
-        try:
-            msg = response['message']
-            print(msg)
-        except:
-            print(url)
-            commit_df = pd.DataFrame.from_records(response)[0:max_commit_count]
-            commit = commit_df[COMMIT]
-            author = commit_df[AUTHOR]
-            committer = commit_df[COMMITTER]
-
-            nested_author = commit.apply(lambda x: x[AUTHOR] if x is not None else '')
-            nested_committer = commit.apply(lambda x: x[COMMITTER] if x is not None else '')
-            
-            commit_df[AUTHOR_NAME] = nested_author.apply(lambda x: x[NAME] if x is not None else '')
-            commit_df[AUTHOR_EMAIL] = nested_author.apply(lambda x: x[EMAIL] if x is not None else '')
-            commit_df[COMMITTER_NAME] = nested_committer.apply(lambda x: x[NAME] if x is not None else '')
-            commit_df[COMMITTER_EMAIL] = nested_committer.apply(lambda x: x[EMAIL] if x is not None else '')
-
-            commit_df[AUTHOR_USERNAME] = author.apply(lambda x: x[LOGIN] if x is not None else '')
-            commit_df[AUTHOR_URL] = author.apply(lambda x: x[HTML_URL] if x is not None else '')
-            commit_df[COMMITTER_USERNAME] = committer.apply(lambda x: x[LOGIN] if x is not None else '')
-            commit_df[COMMITTER_URL] = committer.apply(lambda x: x[HTML_URL] if x is not None else '')
-            
-            commit_df[COMMIT_MESSAGE] = commit.apply(lambda x: x[MESSAGE] if x is not None else '')
-            commit_df = commit_df.rename(columns={HTML_URL:COMMIT_URL,SHA:COMMIT_HASH})
-
-            
-            commit_df = commit_df[[COMMIT_HASH,COMMIT_URL,COMMIT_MESSAGE,AUTHOR_NAME,AUTHOR_EMAIL,
-            AUTHOR_USERNAME,AUTHOR_URL,COMMITTER_NAME,COMMITTER_EMAIL,COMMITTER_USERNAME,COMMITTER_URL]]
-            repo_name = df[NAME][k]
-            try:
-                os.mkdir(ANSWER2+repo_name)
-            except Exception as ex:
-                pass
-
-            commit_df.to_csv(ANSWER2+repo_name+COMMITS_CSV, encoding=UTF8)
-            k+=1
-
+    create_commits_csv(df, max_commit_count)
     df.to_csv(ANSWER1, encoding=UTF8)
 
-get_repos(4, 4)
+
+def create_commits_csv(df, max_commit_count):
+    """Create Commits Csv"""
+    k = 0
+    for url in df[COMMITS_URL]:
+        url = url.replace('{/sha}', '')
+        response = requests.get(url, HEADERS).json()
+        repo_name = df[NAME][k]
+        if response[MESSAGE] is not None:
+            logging.WARNING(str(datetime.datetime.now())+': '+repo_name+': Empty Repository')
+        else:
+            print(repo_name)
+            print(response)
+            commit_df = pd.DataFrame.from_records(
+                    response)[0:max_commit_count]
+            util_map = create_util_df_map(commit_df)
+                
+            for i, j in COMMIT_COL_MAP.items():
+                temp_df = get_temp_df(i,util_map)
+                add_new_col(i, j, temp_df, commit_df)
+
+            commit_df = commit_df.rename(columns=COMMIT_RENAME_MAP)
+            commit_df = commit_df[COMMIT_COLUMNS]
+            print('lol')
+            create_directory(df, k, repo_name)
+            commit_df.to_csv(ANSWER2+repo_name+COMMITS_CSV, encoding=UTF8)
+        k += 1
+
+
+def create_util_df_map(commit_df):
+    """Create Util Map"""
+    commit = commit_df[COMMIT]
+    return {
+        COMMIT: commit,
+        AUTHOR: commit_df[AUTHOR],
+        COMMITTER: commit_df[COMMITTER],
+        NESTED_AUTHOR: commit.apply(
+            lambda x: x[AUTHOR] if x is not None else ''),
+        NESTED_COMMITTER: commit.apply(
+            lambda x: x[COMMITTER] if x is not None else '')
+    }
+
+
+def create_directory(df, k, repo_name):
+    """Create Directory"""
+    try:
+        os.mkdir(ANSWER2+repo_name)
+    except Exception as ex:
+        logging.info(str(datetime.datetime.now())+': '+df[NAME][k]+': '+str(ex))
+
+
+def add_new_col(required_name, present_name, temp_df, final_df):
+    """Add new Columns in the Dataframe"""
+    final_df[required_name] = temp_df.apply(
+        lambda x: x[present_name] if x is not None else '')
+
+def get_temp_df(i,util_map):
+    """Get temp df"""
+    if i == AUTHOR_NAME or i == AUTHOR_EMAIL:
+        return util_map[NESTED_AUTHOR]
+    elif i == COMMITTER_NAME or i == COMMITTER_EMAIL:
+        return util_map[NESTED_COMMITTER]
+    elif i == AUTHOR_USERNAME or i == AUTHOR_URL:
+        return util_map[AUTHOR]
+    elif i == COMMITTER_USERNAME or i == COMMITTER_URL:
+        return util_map[COMMITTER]
+    elif i == COMMIT_MESSAGE:
+        return util_map[COMMIT]
+
+if __name__ == '__main__':
+    """Main Function Change the variables"""
+    make_api_call(5, 4)
--- constants.py
+++ constants.py
@@ -1,27 +1,41 @@
+# PATH TO CHANGE:
+# ASSIGNMENT 1
+import os
+cwd= os.getcwd()
+ANSWER1 = str(cwd) +'/created_data/repos.csv'
+ANSWER2 = str(cwd)+'/created_data/'
+
+# ASSIGNMENT 2
+PATH = str(cwd)
+DATA_PATH = str(cwd)+'/datasets-master/'
+
+
 # A1
 URL = 'https://api.github.com/repositories?since=10000'
-TOKEN_VALUE =  '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'
-AUTHERIZATION= 'Authorization'
+TOKEN_VALUE = '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'
+AUTHERIZATION = 'Authorization'
 TOKEN = 'token '
 AUTH_TOKEN = 'auth_token '
-ANSWER1 = '/media/cph0r/Storage/steeyleye/created_data/repos.csv'
-ANSWER2 = '/media/cph0r/Storage/steeyleye/created_data/'
+
 COMMITS_CSV = '/commits.csv'
+NESTED_AUTHOR = 'nested_author'
+NESTED_COMMITTER = 'nested_committer'
+
 
 # COLUMNS NAME
 DESCRIPTION = 'description'
-ID ='id'
-NAME ='name'
+ID = 'id'
+NAME = 'name'
 FULL_NAME = 'full_name'
 NODE_ID = 'node_id'
-PRIVATE =  'private'
+PRIVATE = 'private'
 FORK = 'fork'
 OWNER = 'owner'
 OWNER_USERNAME = 'owner_username'
 TYPE = 'type'
 HTML_URL = 'html_url'
 LOGIN = 'login'
-SITE_ADMIN  = 'site_admin'
+SITE_ADMIN = 'site_admin'
 OWNER_ADMIN = 'owner_site_admin'
 OWNER_URL = 'owner_url'
 OWNER_TYPE = 'owner_type'
@@ -43,16 +57,43 @@ AUTHOR_USERNAME = 'author_username'
 AUTHOR_EMAIL = 'author_email'
 AUTHOR_URL = 'author_url'
 
-COMMITTER_NAME ='committer_name'
+COMMITTER_NAME = 'committer_name'
 COMMITTER_USERNAME = 'committer_username'
-COMMITTER_EMAIL ='committer_email'
+COMMITTER_EMAIL = 'committer_email'
 COMMITTER_URL = 'committer_url'
 
 # A2
-PATH = '/media/cph0r/Storage/steeyleye/'
-DATA_PATH = '/media/cph0r/Storage/steeyleye/datasets-master/'
+
 CSV = '.csv'
 USER_DATA = 'user_dataset'
 ACTIVITY = 'activity_'
 NO_OF_ACTIVITY_FILES = 5
 UTF8 = 'utf-8'
+
+HEADERS = {AUTHERIZATION: AUTH_TOKEN+TOKEN_VALUE}
+
+# ARRAYS AND MAPS
+
+DF_MAP = {OWNER_TYPE: TYPE, OWNER_URL: HTML_URL,
+          OWNER_USERNAME: LOGIN, OWNER_ADMIN: SITE_ADMIN}
+REPO_COLUMNS = [ID, NAME, FORK, FULL_NAME, OWNER_URL, OWNER_USERNAME, OWNER_ADMIN,
+                PRIVATE, DESCRIPTION, NODE_ID, HTML_URL, OWNER_TYPE, COMMITS_URL]
+
+COMMIT_COLUMNS = [COMMIT_HASH, COMMIT_URL, COMMIT_MESSAGE, AUTHOR_NAME, AUTHOR_EMAIL,
+                  AUTHOR_USERNAME, AUTHOR_URL, COMMITTER_NAME, COMMITTER_EMAIL, COMMITTER_USERNAME, COMMITTER_URL]
+
+COMMIT_COL_MAP = {AUTHOR_NAME:  NAME,
+                  AUTHOR_EMAIL: EMAIL,
+                  COMMITTER_NAME: NAME,
+                  COMMITTER_EMAIL: EMAIL,
+                  AUTHOR_USERNAME: LOGIN,
+                  AUTHOR_URL: HTML_URL,
+                  COMMITTER_USERNAME: LOGIN,
+                  COMMITTER_URL:  HTML_URL,
+                  COMMIT_MESSAGE: MESSAGE}
+
+COMMIT_RENAME_MAP = {HTML_URL: COMMIT_URL, SHA: COMMIT_HASH}
+
+# ERRORS
+API_LIMIT = 'Api Limit Excedeed'
+FOLDER_ALREADY_EXIST = 'folder already exist'
